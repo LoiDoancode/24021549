f76af41b87a7c44d6648c7b3c5fda9fb685571fc
LoiDoancode <loidoan221106@gmail.com>
2025-05-06 13:46:59 +0700

    collsion

--- check.h
+++ check.h
@@ -2,7 +2,7 @@
 #define _CHECK__H
 bool checkGameOver(const Block& block, int manX, int manY) {
     return (block.x < manX + 180 && block.x + block.width > manX &&
-            block.y < manY + 195 && block.y + block.height > manY) ||
-           (block.y >= SCREEN_HEIGHT);
+            block.y < manY + 160 && block.y + block.height > manY+80) ||
+           (block.y >= SCREEN_HEIGHT+80);
 }
 #endif
